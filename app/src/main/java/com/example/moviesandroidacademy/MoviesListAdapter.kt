6d0cf7ce62b50787f9b89b0c3c2f53e1feedaf9b
Nilsolk <nilsolk.bat@gmail.com>
2023-03-01 19:39:20 +0300

    XML markup for RecyclerView

---
+++ app/src/main/java/com/example/moviesandroidacademy/MoviesListAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.moviesandroidacademy
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MoviesListAdapter : RecyclerView.Adapter<MoviesViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MoviesViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+class MoviesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
